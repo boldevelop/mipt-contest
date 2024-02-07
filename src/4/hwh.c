13e8a058ca77790ce6ec5b9d260e2f07b319e181
boldevelop <boldakov.vladislav@gmail.com>
2024-02-07 20:14:45 +0300

    temp hwh

--- src/4/hwh.c
+++ src/4/hwh.c
@@ -1,41 +1,14 @@
 /* 
-h(S)  =  S[0]  +  S[1] * P  +  S[2] * P^2  +  S[3] * P^3  +  ...  +  S[N] * P^N
+    h(S)  =  S[0]  +  S[1] * P  +  S[2] * P^2  +  S[3] * P^3  +  ...  +  S[N] * P^N
 
-hi(x) = ((ax + b) % p) % m
-a, b - любые
-m: мощность хэша
-p: простое число
-Строки
-h(c1..cl) = hi( SUM(i, l, ci * r^(l-i)) % p)
+    hi(x) = ((ax + b) % p) % m
+    a, b - любые
+    m: мощность хэша
+    p: простое число
+    Строки
+    h(c1..cl) = hi( SUM(i, l, ci * r^(l-i)) % p)
 
-unsigned hashint(unsigned a, unsigned b, unsigned x) {
-    return (a*x + b) >> (w - M);
-}
-
-djb2
-unsigned long
-hash(unsigned char *str)
-{
-    unsigned long hash = 5381;
-    int c;
-
-    while (c = *str++)
-        hash = ((hash << 5) + hash) + c; // hash * 33 + c 
-
-    return hash;
-}
-
-K&R2
-unsigned hash(char *s)
-{
-    unsigned hashval;
-
-    for (hashval = 0; *s != '\0'; s++)
-        hashval = *s + 31*hashval;
-    return hashval % HASHSIZE;
-}
-
-https://raw.githubusercontent.com/dwyl/english-words/master/words.txt
+    https://raw.githubusercontent.com/dwyl/english-words/master/words.txt
 */
 
 #include <stdio.h>
@@ -43,12 +16,11 @@ https://raw.githubusercontent.com/dwyl/english-words/master/words.txt
 #include <string.h>
 #include <assert.h>
 #include <ctype.h>
-/* typedef long long i64; size_t (unsigned long) */
 
-// #define HASHSIZE 65536
-#define HASHSIZE 466550
+/* 22 24 30 22 18 26 16 28 30 22 18  */
 
-int readint() {
+int readint()
+{
     int n;
     if (scanf("%d", &n) != 1) {
         abort();
@@ -56,152 +28,102 @@ int readint() {
     return n;
 }
 
-void filter_words();
 unsigned pow_mod(unsigned n, unsigned k);
-unsigned a;
-unsigned b;
 
 struct bucket_t {
-    struct bucket_t* next;
-    char* key;
+    struct bucket_t *next;
+    char *key;
     int d;
 };
-typedef struct bucket_t Bucket; 
+typedef struct bucket_t Bucket;
 
 typedef struct {
     unsigned a;
     unsigned b;
     unsigned p;
     unsigned s;
-    Bucket** buckets;
+    Bucket **buckets;
 } HashMap;
 
-HashMap* init_hm(int strl);
-int find_hm(HashMap* hm, char* key);
-void insert_hm(HashMap* hm, char* key);
-void destroy_hm(HashMap* hm);
+HashMap *init_hm(int strl);
+int find_hm(HashMap * hm, char *key);
+void insert_hm(HashMap * hm, char *key);
+void destroy_hm(HashMap * hm);
 
-void flush() {
+void flush()
+{
     while (getchar() != '\n');
 }
 
-int read_str(int strl) {
+int read_word(int strl, char *buf)
+{
     int c;
-    char buf[1024];
     int bi = 0;
-    int read = 0;
     do {
         c = getchar();
         strl--;
+
         if (isspace(c) || c == EOF) {
             buf[bi++] = '\0';
-            read++;
-            // printf("out: %s\n", buf);
-            bi = 0;
-            if (c == EOF) {
-                break;
-            }
-        } else {
-            buf[bi++] = c;
+            break;
         }
-    } while (strl >= 0);
-    return read;
-    // printf("READ: %d\n", read);
-}
 
-int main() {
-    char wrds[10][512];
-    int i = 0;
-    int c;
-    // srand(738547485u);
-    // a = rand();
-    // b = rand();
-    // printf("%d %d\n", a, b);
-
-    #if 0
-    int a, l;
-    a = readint();
-    l = readint();
-    #endif
-
-    int a, l, s;
-    int t1, t2;
-    char buf[1024];
-    int bi = 0;
-    a = readint();
-    l = readint();
-    flush();
-    t1 = read_str(l);
-    s = readint();
-    flush();
-    t2 = read_str(s);
-    printf("%d %d %d\n", a, l, s);
-    printf("%d %d\n", t1, t2);
-    return 0;
-    while (getchar() != '\n');
-    do {
-        c = getchar();
-        if (isspace(c) || c == EOF || c == '\n') {
-            buf[bi++] = '\0';
-            strcpy(*(wrds + i), buf);
-            i++;
-            bi = 0;
-            if (c == EOF || i == 3) {
-                break;
-            }
-            continue;
-        }
         buf[bi++] = c;
-        l--;
-    } while (l > 0);
-    // while ((scanf("%s", &word)) == 0) {
-    //     wrds[i++] = word;
-    // }
-    for (int j = 0; j < i; ++j) {
-        printf("%s\n", wrds[j]);
-    }
+    } while (strl > 0);
 
-    // filter_words();
+    return strl;
 }
 
-unsigned hash(char *s)
+#define BUF_S 1024
+
+int *alloc_arri(size_t s)
 {
-    unsigned h;
-
-    for (h = 0; *s != '\0'; s++) {
-        h = *s + 31*h;
+    int *buf = calloc(s, sizeof(int));
+    if (!buf) {
+        abort();
     }
-
-    return h % HASHSIZE;
+    return buf;
 }
 
+int main()
+{
+    int exp_a, text_l;
+    char buf[BUF_S];
+    int *answers;
+    int ind = 0;
+    HashMap *hm;
 
-#define MAX(l, r) (l) < (r) ? (r) : (l);
+    srand(738547485u);
 
-void collision(int* b) {
-    int c = 0, max = 0;
-    for (int i = 0; i < HASHSIZE; ++i) {
-        c += b[i];
-        max = MAX(max, b[i]);
+    exp_a = readint();
+    answers = alloc_arri(exp_a);
+    text_l = readint();
+    hm = init_hm(text_l);
+
+    text_l++;
+    flush();
+    do {
+        text_l = read_word(text_l, buf);
+        insert_hm(hm, buf);
+    } while (text_l);
+
+    text_l = readint();
+    text_l++;
+    flush();
+    do {
+        text_l = read_word(text_l, buf);
+        answers[ind++] = find_hm(hm, buf);
+    } while (text_l);
+
+    for (int i = 0; i < exp_a; ++i) {
+        printf("%d ", answers[i]);
     }
-    printf("strhash\n");
-    printf("HS: %d\n", HASHSIZE);
-    printf("c: %d\n", c/HASHSIZE);
-    printf("max: %d\n", max);
-}
-unsigned strhash(char* str);
-void filter_words() {
-    char line[256];
-    int *buf = calloc(HASHSIZE, sizeof(int));
-    if (!buf) abort();
-    while(fgets(line, sizeof(line), stdin) != NULL) {
-        unsigned h;
-        h = strhash(line);
-        buf[h]++;
-    }
-    
-    collision(buf);
-    free(buf);
+    printf("\n");
+
+    destroy_hm(hm);
+    free(answers);
+
+    return 0;
 }
 
 /* n^k mod sizeof(unsigned) */
@@ -220,37 +142,21 @@ unsigned pow_mod(unsigned n, unsigned k)
     return prod;
 }
 
-unsigned strhash(char* str) {
-    unsigned p = 10000019;
-    // unsigned p = 2 * 1000000000 + 11;
-    unsigned r = 1;
-    unsigned sum = 0;
-
-    char *c = str;
-    while (*c != '\0') {
-        sum += (*c) * r;
-        r *= 17;
-        c++;
-    }
-    sum %= p;
-
-    sum = (a * sum + b) % p;
-    return sum % HASHSIZE;
-}
-
 #define MAX_POW_2 10
 
-unsigned rand_range(unsigned l, unsigned r) {
+unsigned rand_range(unsigned l, unsigned r)
+{
     return l + rand() / (RAND_MAX / (r - l + 1u) + 1u);
 }
 
-HashMap* alloc_hm(unsigned p, unsigned s);
-Bucket* alloc_bucket(char* key);
+HashMap *alloc_hm(unsigned p, unsigned s);
+Bucket *alloc_bucket(char *key);
 
-HashMap* init_hm(int strl) {
+HashMap *init_hm(int strl)
+{
     static unsigned pow_2[MAX_POW_2];
     /*  words in str max 'strl / 2',
-        so take 'words*2' */
+       so take 'words*2' */
     unsigned posible_count_words = strl;
     unsigned s = 0;
     unsigned p = 10000019;
@@ -271,7 +177,8 @@ HashMap* init_hm(int strl) {
     return alloc_hm(p, s);
 }
 
-unsigned hs(HashMap* hm, char* key) {
+unsigned hs(HashMap * hm, char *key)
+{
     unsigned r = 1;
     unsigned sum = 0;
 
@@ -287,9 +194,10 @@ unsigned hs(HashMap* hm, char* key) {
     return sum;
 }
 
-int find_hm(HashMap* hm, char* key) {
+int find_hm(HashMap * hm, char *key)
+{
     unsigned ind;
-    Bucket* cur;
+    Bucket *cur;
 
     assert(hm);
 
@@ -308,12 +216,13 @@ int find_hm(HashMap* hm, char* key) {
     return 0;
 }
 
-void insert_hm(HashMap* hm, char* key) {
+void insert_hm(HashMap * hm, char *key)
+{
     unsigned ind;
-    Bucket* cur, *prev;
+    Bucket *cur, *prev;
 
     assert(hm);
-    
+
     ind = hs(hm, key) % hm->s;
     cur = hm->buckets[ind];
     if (!cur) {
@@ -333,37 +242,44 @@ void insert_hm(HashMap* hm, char* key) {
     prev->next = alloc_bucket(key);
 }
 
-HashMap* alloc_hm(unsigned p, unsigned s) {
-    HashMap* hm = calloc(1, sizeof(HashMap));
-    if (!hm) abort();
+HashMap *alloc_hm(unsigned p, unsigned s)
+{
+    HashMap *hm = calloc(1, sizeof(HashMap));
+    if (!hm)
+        abort();
     hm->p = p;
     hm->a = rand_range(1, p - 1);
     hm->b = rand_range(0, p - 1);
     hm->s = s;
-    hm->buckets = calloc(s, sizeof(Bucket*));
-    if (!hm->buckets) abort();
+    hm->buckets = calloc(s, sizeof(Bucket *));
+    if (!hm->buckets)
+        abort();
     return hm;
 }
 
-Bucket* alloc_bucket(char* key) {
-    Bucket* b = calloc(1, sizeof(Bucket));
-    if (!b) abort();
+Bucket *alloc_bucket(char *key)
+{
+    Bucket *b = calloc(1, sizeof(Bucket));
+    if (!b)
+        abort();
     b->key = key;
     b->d = 1;
     return b;
 }
 
-void free_buckets(Bucket* cur) {
+void free_buckets(Bucket * cur)
+{
     while (cur) {
-        Bucket* tmp = cur->next;
+        Bucket *tmp = cur->next;
         free(cur);
         cur = tmp;
     }
 }
 
-void destroy_hm(HashMap* hm) {
+void destroy_hm(HashMap * hm)
+{
     for (unsigned i = 0; i < hm->s; ++i) {
-        Bucket* cur = hm->buckets[i];
+        Bucket *cur = hm->buckets[i];
         free_buckets(cur);
     }
     free(hm->buckets);

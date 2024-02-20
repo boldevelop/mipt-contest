1a25ab6654ad35db06c0d226afba2596b77442ba
boldevelop <boldakov.vladislav@gmail.com>
2024-02-20 19:42:09 +0300

    temp

--- src/5/rxb.c
+++ src/5/rxb.c
@@ -2,7 +2,62 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <regex.h>
+
+const char* MAIL_RE = "^([a-z]|[A-Z]|[0-9]|.)+@[a-zA-Z]*\\.[a-zA-Z]*$";
+const char* TEXT_MAIL = "a.test98@qwe.com";
+
+#define ARRAY_SIZE(array) (sizeof((array)) / sizeof((array)[0]))
+char buf[1024];
+
+void is_valid_email(const regex_t* rx, const char* email) {
+    regmatch_t pmatch[3];
+#ifdef DEBUG
+    regoff_t offset, length;
+#endif
+
+    int ok;
+
+    int ret = regexec(rx, email, ARRAY_SIZE(pmatch), pmatch, 0);
+    if (ret && ret != REG_NOMATCH) {
+        regerror(ret, rx, buf, sizeof(buf));
+        fprintf(stderr, "Error: regexec: %s\n", buf);
+        exit(EXIT_FAILURE);
+    }
+
+    ok = ret == 0;
+#ifdef DEBUG
+    if (ok) {
+        offset = pmatch[0].rm_so;
+        length = pmatch[0].rm_eo - pmatch[0].rm_so;
+
+        printf("%s\n", email);
+        printf("%.*s\n", (int)length, email + offset);
+
+        for (unsigned long j = 0; j < ARRAY_SIZE(pmatch); j++) {
+            if (pmatch [j].rm_so == -1)
+                break;
+        }
+    }
+#endif
+
+    printf("%d\n", ok);
+}
+
+
+void compile_regex(regex_t* rx, const char* regexstr) {
+    int ret = regcomp(rx, regexstr, 0);
+    if (ret != 0) {
+        regerror(ret, rx, buf, sizeof(buf));
+	    fprintf(stderr, "Error: regcomp: %s\n", buf);
+        exit(EXIT_FAILURE);
+    }
+}
 
 int main() {
-    printf("1\n");
+    regex_t rx;
+    compile_regex(&rx, MAIL_RE);
+    scanf("%s", buf);
+    is_valid_email(&rx, buf);
+    regfree(&rx);
 }

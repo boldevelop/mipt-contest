922018e30a9e99aa4a2abfa4c1222cd6ab5f859f
boldevelop <boldakov.vladislav@gmail.com>
2024-04-03 13:01:24 +0300

    agr

--- src/6/agr.s
+++ src/6/agr.s
@@ -97,62 +97,46 @@ foo:
     lw      t3, 0(t4)
     mv      a6, a0
     sext.w  a7, a2
+
     bgeu    a1, a2, .L7
 
     slli    t1, a7, 2   
-
     add     a5, a0, t1  
-
     mv      a4, a7      
 .L6: 
-
     lw      a3, 0(a5)   
-
     add     a2, a6, t1  
-
     addiw   a4, a4, -1  
 
     ble     a3, t3, .L5 
 
     lw      a0, 0(a2)   
-
     addiw   a7, a7, -1  
 
     sw      a3, 0(a2)   
-
     sw      a0, 0(a5)   
 
     slli    t1, a7, 2   
 .L5: 
-
     addi    a5, a5, -4  
 
     bltu    a1, a4, .L6 
 
     lw      t3, 0(t4)   
-
     add     a6, a6, t1  
-
     lw      a5, 0(a6)   
 
+    sext.w  a0, a7    
+
     sw      t3, 0(a6)   
-
-    sext.w  a0, a7      
-
     sw      a5, 0(t4)   
-    ret                 
-                        
+    ret
 .L7: 
-
     slli    t1, a7, 2
-
     add     a6, a6, t1
-
     lw      a5, 0(a6)
-
-    sw      t3, 0(a6)
-
     mv      a0, a2
 
+    sw      t3, 0(a6)
     sw      a5, 0(t4)
     ret
